d73c88a2936fa4196b0ba5cfcd2151b19e2f5375
Владислав Наумов <vlad_naumov_21@MacBook-Air-Vladislav.local>
2023-10-12 23:57:23 +0300

    fixing

--- california_housing/train.py
+++ california_housing/train.py
@@ -24,37 +24,8 @@ def main():
     )
     trainer.fit(train_loader=train_dataloader, val_loader=val_dataloader, epochs=100)
     trainer.print_score()
-    torch.save(model.state_dict(), "model")
+    torch.save(model.state_dict(), "../models/model")
 
 
 if __name__ == '__main__':
     main()
-
-# model = RegressionModel()
-# loss_fn = nn.MSELoss()  # mean square error
-# optimizer = optim.Adam(model.parameters(), lr=0.0001)
-# train_dataset = get_train_dataset()
-# train_dataloader = DataLoader(train_dataset, batch_size=50)
-
-# n_epochs = 100
-# train_loss = []
-# test_loss = []
-# best_mse = np.inf
-# for _epoch in tqdm(range(n_epochs)):
-#     train_loss_epoch = []
-#     model.train()
-#     for X_batch, y_batch in train_dataloader:
-#         # forward pass
-#         y_pred = model(X_batch)
-#         loss = loss_fn(y_pred, y_batch)
-#         # backward pass
-#         optimizer.zero_grad()
-#         loss.backward()
-#         train_loss_epoch.append(loss.item())
-#         # update weights
-#         optimizer.step()
-#         optimizer.zero_grad()
-#     train_loss.append(np.mean(train_loss_epoch))
-
-# if __name__ == '__main__':
-#     torch.save(model.state_dict(), "model")
