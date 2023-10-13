10ab32ce8c715b8ae15642f856efd91c14092793
naumov-vladis <naumov-vladis@yandex-team.ru>
2023-10-13 18:03:13 +0300

    add hydra config

--- california_housing/train.py
+++ california_housing/train.py
@@ -6,23 +6,38 @@ from model import RegressionModel
 from torch.utils.data import DataLoader
 from trainer import Trainer
 from transforms import normalize_data
+import hydra
+from omegaconf import DictConfig
 
 
-def main():
+@hydra.main(version_base=None, config_path="../conf", config_name="config")
+def main(config: DictConfig):
     model = RegressionModel()
     train_dataset = CaliforniaDataset("../data/train_data.csv")
     val_dataset = CaliforniaDataset("../data/val_data.csv")
     train_dataset, val_dataset = normalize_data(train_dataset, val_dataset)
     criterion = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.0001)
-    train_dataloader = DataLoader(train_dataset, batch_size=50)
-    val_dataloader = DataLoader(val_dataset, batch_size=50)
+    optimizer = optim.Adam(
+        model.parameters(), 
+        lr=config["training"]["learning_rate"]
+    )
+    train_dataloader = DataLoader(
+        train_dataset, 
+        batch_size=config["training"]["batch_size"]
+    )
+    val_dataloader = DataLoader(
+        val_dataset, 
+        batch_size=config["training"]["batch_size"]
+    )
     trainer = Trainer(
         model=model,
         criterion=criterion,
         optimizer=optimizer,
     )
-    trainer.fit(train_loader=train_dataloader, val_loader=val_dataloader, epochs=100)
+    trainer.fit(
+        train_loader=train_dataloader, 
+        val_loader=val_dataloader, 
+        epochs=config["training"]["num_epochs"])
     trainer.print_score()
     torch.save(model.state_dict(), "../models/model")
 
